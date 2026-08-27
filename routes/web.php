9dcf9036a38e860efe9f0fe79fbc4a0ff829fb51
Martin Cortes <62093631+tincortes@users.noreply.github.com>
2026-08-27 03:29:31 -0600

    edit pagina principal

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('portada');   // Página principal
-});
-
-Route::get('/about', function () {
-    return view('about');     // Sobre mí
-});
-
-Route::get('/projects', function () {
-    return view('projects');  // Proyectos
-});
-
-Route::get('/contact', function () {
-    return view('contact');   // Contacto
-});
+Route::get('/', fn() => view('portada'));
+Route::get('/about', fn() => view('about'));
+Route::get('/projects', fn() => view('projects'));
+Route::get('/contact', fn() => view('contact'));
