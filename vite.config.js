f751bb3e550b3b00b7972c398a6261c3c7b4ac27
Martin Cortes <62093631+tincortes@users.noreply.github.com>
2026-08-31 02:05:51 -0600

    subiendo práctica tres

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/portfolio.css',
-                'resources/js/portfolio.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+    },
 });
