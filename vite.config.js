1bfa490927bdc76c33f616efa67bba0de147ca78
Martin Cortes <62093631+tincortes@users.noreply.github.com>
2026-09-03 01:17:44 -0600

    Setup completo Laravel + Vite en Heroku

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,19 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
     build: {
         outDir: 'public/build',
+    },
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
     },
 });
