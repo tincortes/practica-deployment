79e4ee54775c56d65da4b7df26a9d2e324b501ec
Martin Cortes <62093631+tincortes@users.noreply.github.com>
2026-09-06 22:24:32 -0600

    Configurar Laravel para mostrar portafolio en Heroku

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
@@ -9,4 +15,3 @@ export default defineConfig({
         outDir: 'public/build',
     },
 });
-
