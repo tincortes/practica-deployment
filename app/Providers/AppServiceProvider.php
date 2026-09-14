39fa2f5093bd12c8baf70d6058692a8c44f1b88d
Martin Cortes <62093631+tincortes@users.noreply.github.com>
2026-09-14 10:58:19 -0600

    Forzando assets a HTTPS en producción

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,24 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 👈 este es el correcto
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-      if (config('app.env') === 'production') {
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
-        }  
+        }
     }
 }
